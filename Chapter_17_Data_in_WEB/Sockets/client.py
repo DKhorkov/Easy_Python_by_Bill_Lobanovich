1d2b7635fa55d0ed6e94355541ce846595f2755d
DKhorkov <dkhorkov.dev@gmail.com>
2022-10-02 14:17:14 +0300

    Created console-chat

--- Chapter_17_Data_in_WEB/Sockets/client.py
+++ Chapter_17_Data_in_WEB/Sockets/client.py
@@ -1,4 +1,6 @@
+import os
 import socket
+import threading
 
 
 class Client:
@@ -7,21 +9,26 @@ class Client:
         self.server_addr = server_addr
         self.server_port = port
 
-    def connect_to_server(self):
+    def thread_for_listen_for_messages(self, client):
         while True:
-            self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Создаем клиента на IP/TCP
-            self.client.connect((self.server_addr, self.server_port))
+            self.message = client.recv(4096).decode('utf-8')
+            print(f"\r\r{self.message}\nMe: ", end='')
 
+    def connect_to_server(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Создаем клиента на IP/TCP
+        self.client.connect((self.server_addr, self.server_port))
+        self.client.send('__join_server'.encode('utf-8'))
+        threading.Thread(target=self.thread_for_listen_for_messages, args=(self.client,)).start()
+        while True:
             self.data = input('Me: ')
             if self.data.lower() == 'q':
                 break
-            self.client.sendall(self.data.encode('utf-8'))
-            self.answer = self.client.recv(1024).decode('utf-8')
-            print(f'Server provided us with an answer: "{self.answer}"')
+            self.client.send(self.data.encode('utf-8'))
         self.client.close()
 
 
 if __name__ == '__main__':
+    os.system('clear')  # Очищаем консоль перед чатом
     client = Client("", 5555)
     client.connect_to_server()
     
--- Chapter_17_Data_in_WEB/Sockets/server.py
+++ Chapter_17_Data_in_WEB/Sockets/server.py
@@ -6,24 +6,42 @@ class Server:
     def __init__(self, address, port):
         self.address = address
         self.port = port
+        self.clients = []
 
-    def run_server(self):
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Создаем сервер на IP/TCP
-        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # При перезапуске сервера не ждем времени на получение пакетов
+    def create_server(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Создаем сервер на IP/UDP
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,
+                               1)  # При перезапуске сервера не ждем времени на получение пакетов
         self.server.bind((self.address, self.port))  # Подключаемся к адресу
-        self.server.listen(2)  # И начинаем слушать два подключения
         print(f'Server with address "{self.address}" and port "{self.port}" is running and waiting for connecting')
 
-        while True:
-            self.client, self.client_addr = self.server.accept()
-            print(f'Client "{self.client_addr}" connected to server')
+    def check_client_on_existence(self, client_addr):
+        if client_addr not in self.clients:
+            self.clients.append(client_addr)
+            print(self.clients)
 
-            self.data = self.client.recv(1024).decode('utf-8')  # Получаем от клиента инфу
-            if not self.data:
-                break
-            self.answer = f'Thanks for sending us next data : {self.data}'
-            self.client.send(self.answer.encode('utf-8'))
-            print(f'Got "{self.data}" from client and send answer to him')
+    def send_client_message_to_others(self):
+        self.answer = f"Client{self.client_id}: {self.message.decode('utf-8')}"
+        for client in self.clients:
+            if self.client_addr == client:
+                continue
+            self.server.sendto(self.answer.encode('utf-8'), client)
+
+    def run_server(self):
+        self.create_server()
+        while True:
+            self.message, self.client_addr = self.server.recvfrom(4096)
+            self.check_client_on_existence(self.client_addr)
+
+            if not self.message:
+                continue
+
+            self.client_id = self.client_addr[1]
+            if self.message.decode('utf-8') == '__join_server':
+                print(f'Client{self.client_id} has joined the chat!')
+                continue
+
+            self.send_client_message_to_others()
         self.server.close()
 
 
